cddb0c8153dfc0ff4b6f39eeee242fd659e5d821
Лев <maymailforme@gmail.com>
2026-09-15 13:06:28 +0300

    Update Movie.cs

--- Movies/Models/Movie.cs
+++ Movies/Models/Movie.cs
@@ -1,10 +1,27 @@
-﻿namespace Movies.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        //[StringLength(50, MinimumLength=2)]
+        //[MinLength(2), MaxLength(50)]
+        [MinLength(2, ErrorMessage = "Слишком мало"), MaxLength(50, ErrorMessage = "Слишком много")]
+        [Display(Name = "Название")]
         public string Title { get; set; }
+
+
+        //[RangeAttribute(typeof(DateOnly), "1888-10-14", "2128-12-31")]
+        [RangeAttribute(typeof(DateOnly), "1888-10-14", "2128-12-31", ErrorMessage = "Тогда кино либо еще не снимали, либо уже ничего не снимут")]
+        [Display(Name = "Дата выхода")]
         public DateOnly ReleaseDate { get; set; }
+
+
+        [DisplayName("Жанр")]
         public string? Gener {  get; set; }
         public string? URL { get; set; }
         public string Psoter { get; set; }
