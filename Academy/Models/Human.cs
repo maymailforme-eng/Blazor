087a7773fbb7db6bfa018db6d6912650811ad91b
Лев <maymailforme@gmail.com>
2026-09-24 19:17:18 +0300

    Lesson 29 - student model Done

---
+++ Academy/Models/Human.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models
+{
+    public class Human
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Фамилия содержит недопустимый символ")]
+        public string last_name { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Имя содержит недопустимый символ")]
+        public string first_name { get; set; }
+
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Отчество содержит недопустимый символ")]
+        public string? middle_name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [RangeAttribute(typeof(DateOnly), "1950-01-01", "2020-01-01")]
+        DateOnly birth_date { get; set; }
+
+        public string? email { get; set; }
+        public string? phone { get; set; }
+        public byte[]? photo { get; set; }
+
+    }
+}
